04367af594c255d172b276cf914a1cbb38f064c2
ILDUS <ildus6696@gmail.com>
2024-03-30 14:08:47 +0300

    Zadanie2

--- DZ4Zad1.go
+++ DZ4Zad1.go
@@ -42,14 +42,46 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"id": newUser.ID})
 }
 
+type MakeFriendsRequest struct {
+	SourceID string `json:"source_id"`
+	TargetID string `json:"target_id"`
+}
+
+func makeFriendsHandler(w http.ResponseWriter, r *http.Request) {
+	var request MakeFriendsRequest
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sourceUser, sourceExists := users[request.SourceID]
+	targetUser, targetExists := users[request.TargetID]
+
+	if !sourceExists || !targetExists {
+		http.Error(w, "One or both users do not exist", http.StatusNotFound)
+		return
+	}
+
+	sourceUser.Friends = append(sourceUser.Friends, targetUser.Name)
+	targetUser.Friends = append(targetUser.Friends, sourceUser.Name)
+
+	users[request.SourceID] = sourceUser
+	users[request.TargetID] = targetUser
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s и %s теперь друзья", sourceUser.Name, targetUser.Name)
+}
+
 func main() {
 	http.HandleFunc("/create", createUserHandler)
+	http.HandleFunc("/make_friends", makeFriendsHandler)
 
 	fmt.Println("Starting server on :8080...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// curl.exe -d "{\"name\":\"Ildus\",\"age\":44,\"friends\":[\"3\"]}" -H "Content-Type: application/json" -X POST http://localhost:8080/create
-	// curl.exe -d "{\"name\":\"Alena\",\"age\":42,\"friends\":[\"1\"]}" -H "Content-Type: application/json" -X POST http://localhost:8080/create
 }
